e9b414f9f283e57ca1b503322e2a3da5b68e1d31
Vitor Nickhorn Kroeff <138133399+vitorkroeff@users.noreply.github.com>
2025-02-13 15:47:48 -0300

    Add files via upload

---
+++ app.R
@@ -0,0 +1,47 @@
+library(shiny)
+library(dplyr)
+
+ui <- fluidPage(
+    titlePanel(
+        div("Tratamento Dados CRM", style = "text-align: center;")
+    ),
+    sidebarLayout(
+        sidebarPanel(
+            fileInput("file1", "Escolha um arquivo CSV", accept = ".csv"),
+            downloadButton("downloadData", "Baixar Arquivo Tratado")
+        ),
+        mainPanel(
+            tableOutput("table")
+        )
+    )
+)
+
+server <- function(input, output) {
+    dados_tratados <- reactive({
+        req(input$file1)
+        
+        # Lê o arquivo CSV
+        dados <- read.csv(input$file1$datapath, stringsAsFactors = FALSE)
+        
+        # Tratamento dos dados
+        dados <- dados %>% select(c('customer_name', 'customer_email', 'tracking_code'))
+        dados$customer_name <- sub(" .*", "", dados$customer_name)
+        dados$customer_name <-stringr::str_to_title(dados$customer_name)
+        return(dados)
+    })
+    
+    output$table <- renderTable({
+        dados_tratados()
+    })
+    
+    output$downloadData <- downloadHandler(
+        filename = function() {
+            "dados_tratados.csv"
+        },
+        content = function(file) {
+            write.csv(dados_tratados(), file, row.names = FALSE)
+        }
+    )
+}
+
+shinyApp(ui, server)
